1af429f818e75ba446e188e908021a80bd1316b3
Vinoth Kannan <vinothkannan@vinkas.com>
2016-09-12 14:02:57 +0530

    new: visa trait functions

--- app/Foundation/Auth/HandlesVisa.php
+++ app/Foundation/Auth/HandlesVisa.php
@@ -5,4 +5,23 @@ namespace App\Foundation\Auth;
 trait HandlesVisa
 {
 
+  protected function getSeverUrl() {
+    return url(config('visa.server_url'));
+  }
+
+  public function login() {
+    $loginUrl = $this->getSeverUrl() . '/login';
+    return redirect($loginUrl);
+  }
+
+  public function register() {
+    $registerUrl = $this->getSeverUrl() . '/register';
+    return redirect($registerUrl);
+  }
+
+  public function logout() {
+    $logoutUrl = $this->getSeverUrl() . '/logout';
+    return redirect($logoutUrl);
+  }
+
 }
